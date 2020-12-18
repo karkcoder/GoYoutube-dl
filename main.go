b27cddc7f33858a45536576e437fb3744dc37c68
karkcoder <urkarki@gmail.com>
2020-12-17 19:06:11 -0700

    making generic

--- main.go
+++ main.go
@@ -9,26 +9,26 @@ import (
 	"os/exec"
 )
 
-func youtubeDownloadHandler(w http.ResponseWriter, r *http.Request) {
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	youtubeURL := r.FormValue("youtubeURL")
+	downloadURL := r.FormValue("downloadUrl")
 
 	//Parsing URL
-	_, err := url.ParseRequestURI(youtubeURL)
+	_, err := url.ParseRequestURI(downloadURL)
 
 	if err != nil {
-		fmt.Fprintf(w, "Invalid URL %s\n", youtubeURL)
+		fmt.Fprintf(w, "Invalid URL %s\n", downloadURL)
 	} else {
-		fmt.Fprintf(w, "Downloading %s\n", youtubeURL)
+		fmt.Fprintf(w, "Downloading %s\n", downloadURL)
 
 		goExecutable, _ := exec.LookPath("youtube-dl")
 
 		cmdGoVer := &exec.Cmd{
 			Path:   goExecutable,
-			Args:   []string{goExecutable, youtubeURL},
+			Args:   []string{goExecutable, downloadURL},
 			Stdout: os.Stdout,
 			Stderr: os.Stdout,
 		}
@@ -42,7 +42,7 @@ func youtubeDownloadHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
-	http.HandleFunc("/downloadyoutube", youtubeDownloadHandler)
+	http.HandleFunc("/download", downloadHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
